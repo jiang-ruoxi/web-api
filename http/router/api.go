861963cd198ede058ecd789012778e739c78e029
XingRongjiang <xingrongjiang01@163.com>
2024-06-19 15:58:22 +0800

    路由缓存

--- http/router/api.go
+++ http/router/api.go
@@ -19,25 +19,24 @@ func Api(r *gin.RouterGroup) {
 	chineseHandler := api_handler.NewChineseHandler()
 	{
 		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getNavList", routerCache(common.RedisURL_CACHE), chineseHandler.ChineseGetNavList)
-		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getList", chineseHandler.ChineseGetBookList)
-		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getBookInfo", chineseHandler.ChineseGetBookInfo)
+		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getList", routerCache(common.RedisURL_CACHE), chineseHandler.ChineseGetBookList)
+		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getBookInfo", routerCache(common.RedisURL_CACHE), chineseHandler.ChineseGetBookInfo)
 	}
 
 	albumHandler := api_handler.NewAlbumHandler()
 	{
-		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getAlbumList", albumHandler.AlbumGetList)
-		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getAlbumListInfo", albumHandler.AlbumGetListInfo)
-		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getAlbumInfo", albumHandler.AlbumGetInfo)
+		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getAlbumList", routerCache(common.RedisURL_CACHE), albumHandler.AlbumGetList)
+		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getAlbumListInfo", routerCache(common.RedisURL_CACHE), albumHandler.AlbumGetListInfo)
+		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/chinese/getAlbumInfo", routerCache(common.RedisURL_CACHE), albumHandler.AlbumGetInfo)
 	}
 
 	poetryHandler := api_handler.NewPoetryHandler()
 	{
-		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/poetry/cheng/getList", poetryHandler.PoetryGetChengList)
-		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/poetry/cheng/getPoetryInfo", poetryHandler.PoetryGetChengInfo)
-		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/poetry/school/getList", poetryHandler.PoetryGetSchoolList)
+		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/poetry/cheng/getList", routerCache(common.RedisURL_CACHE), poetryHandler.PoetryGetChengList)
+		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/poetry/cheng/getPoetryInfo", routerCache(common.RedisURL_CACHE), poetryHandler.PoetryGetChengInfo)
+		prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/poetry/school/getList", routerCache(common.RedisURL_CACHE), poetryHandler.PoetryGetSchoolList)
 		//prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/poetry/school/getPoetryInfo", poetryHandler.PoetryGetSchoolInfo)
 		//prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/poetry/junior/getList", poetryHandler.PoetryGetJuniorList)
 		//prefixRouter.Use(middleware.CheckWechatMiddleware()).GET("/poetry/junior/getPoetryInfo", poetryHandler.PoetryGetJuniorInfo)
 	}
-
 }
