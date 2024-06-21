6dc2c2e61ffdfb190478cb991fe5106bc2f404a0
XingRongjiang <xingrongjiang01@163.com>
2024-06-21 13:15:35 +0800

    端口

--- commands/serve.go
+++ commands/serve.go
@@ -11,7 +11,7 @@ import (
 func Serve(c *cli.Context) error {
 
 	// 运行HTTP服务
-	graceful.Start(server.NewHttp(server.Addr(":8088"), server.Router(router.All())))
+	graceful.Start(server.NewHttp(server.Addr(":8082"), server.Router(router.All())))
 
 	graceful.Wait()
 
